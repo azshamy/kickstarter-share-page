6cf72fc59cad7384deeb1e061143ce7310c98042
Leandra Tejedor <lmtejedor@gmail.com>
2014-09-19 22:45:00 -0400

    progress bar changes when you click things but megabus blocks addthis hahaha I miss the train

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 $( document ).ready(function() {
+	var currentProgress = 1;
+	var numClicks = 0;
 
-  $(function() {
-    $( "#progressbar" ).progressbar({
-      value: 37
-    });
+	setProgress(currentProgress);
+
+  $('.js-bargraph').click(function(){
+  	if (numClicks < 3){
+  		numClicks++;
+  		currentProgress = currentProgress + 33;
+  		setProgress(currentProgress);
+  	}
+  	else{
+  	}
   });
 
-
 });
+
+function setProgress(currentProgress){
+  $( "#progressbar" ).progressbar({
+    value: currentProgress
+  });
+}
